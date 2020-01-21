8f920a8c70d297881dcc4ba02d5f7ae58819b123
Mike Sunderlin <msunderlin@gmail.com>
2020-01-20 19:00:31 -0800

    Updates

---
+++ precache-manifest.02b49bd1d6f4c4cecb120460de154d42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c161e8ade52b44a2c5a61836ffad25c5",
+    "url": "/thevintageapron/index.html"
+  },
+  {
+    "revision": "5f4e615781a2b77dd9bb",
+    "url": "/thevintageapron/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "20404e1cc4532ba223ea",
+    "url": "/thevintageapron/static/js/2.744c8376.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/thevintageapron/static/js/2.744c8376.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f4e615781a2b77dd9bb",
+    "url": "/thevintageapron/static/js/main.0737a5fb.chunk.js"
+  },
+  {
+    "revision": "861ee65e355714ab5ae4",
+    "url": "/thevintageapron/static/js/runtime-main.2d514889.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thevintageapron/static/media/logo.5d5d9eef.svg"
+  }
+]);
